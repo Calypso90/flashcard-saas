435d84a6c4d4b91ea7c58e751446a18c4e29275f
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-15 19:56:27 -0400

    chore: Update global styles, add FlashcardSets and Subscription pages

---
+++ app/(navigation)/flashcards/page.jsx
@@ -0,0 +1,10 @@
+import FlashcardSets from '@/app/components/FlashcardSets';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <FlashcardSets />
+    );
+}
+
+export default Page;
---
+++ app/(navigation)/subscription/page.jsx
@@ -0,0 +1,10 @@
+import Subscription from '@/app/components/Subscription';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <Subscription />
+    );
+}
+
+export default Page;
