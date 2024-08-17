1702dda664a665cefcf23d856638c5aad0e6cda2
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-16 23:19:20 -0400

    chore: Update Navbar component with responsive layout and typing animation

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,17 +4,17 @@ import '../globals.css';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between py-4 px-8">
-      <div className="flex">
+        <nav className="flex flex-col sm:flex-row items-center justify-between py-4 px-8">
+      <div className="flex mb-4 sm:mb-0">
         <Link href="/" className="text-2xl tracking-wider">
-        <span className="typing-text">
+          <span className="typing-text">
             {Array.from("Flash\u00A0Wave").map((char, index) => (
               <span key={index} className="typing-letter">{char}</span>
             ))}
           </span>
         </Link>
       </div>
-      <div className="flex items-center space-x-4 gap-4">
+      <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4 gap-4">
         <SignedIn>
           <Link href="/dashboard" className="text-gray-200 text-xl hover:text-blue-300">
             Dashboard
@@ -25,7 +25,7 @@ export default function Navbar() {
           <Link href="/sign-in" className="text-gray-200 hover:text-blue-300">
             Sign In
           </Link>
-          <Link href="/sign-up" className="bg-blue-500  hover:bg-blue-600 text-white px-4 py-2 rounded">
+          <Link href="/sign-up" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
             Sign Up
           </Link>
         </SignedOut>
