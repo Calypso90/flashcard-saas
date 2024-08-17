99a4aee581d3c515b0d1f1547bc6fecbeb9ded96
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-17 00:26:37 -0400

    chore: Update sign-in and sign-up pages with redirect URLs

--- app/(navigation)/sign-in/[[...sign-in]]/page.jsx
+++ app/(navigation)/sign-in/[[...sign-in]]/page.jsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className='flex items-center justify-center flex-col gap-10'>
       <h1 className='text-4xl font-bold mt-20'>Sign In</h1>
-      <SignIn />
+      <SignIn redirectUrl={"/"}/>
     </div>
   );
 }
