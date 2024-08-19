fe5d6cbdb35e52e22a75abf0b4c5d4a0681424d6
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-19 18:32:45 -0400

    chore: Refactor Footer component to use flexbox for centering

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,16 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container mt-10">
-        <div className="row">
-          <div className="col-12">
-            <p className="text-center text-slate-300 py-4">
-              © 2024 Flash Wave. All Rights Reserved.
-            </p>
-          </div>
-        </div>
-      </div>
+    <footer className="flex items-center justify-center">
+      <p className="text-center text-slate-300 py-4">
+        © 2024 Flash Wave. All Rights Reserved.
+      </p>
     </footer>
   );
 };
