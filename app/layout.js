25fb5461631b556cd1f639f414ce0b11ab9a32dc
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-19 19:05:13 -0400

    chore: Add analytics to RootLayout component for tracking user behavior

--- app/layout.js
+++ app/layout.js
@@ -2,25 +2,25 @@
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const metadata = {
   title: "Flash Wave",
   description: "Generate flashcards using AI",
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-      <head>
+        <head>
           <title>{metadata.title}</title>
           <meta name="description" content={metadata.description} />
-          <link rel="icon" href="/favicon.ico" />
         </head>
         <body>
           <Navbar />
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
